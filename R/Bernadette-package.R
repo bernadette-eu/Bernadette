5de7df94b521ba44edf9861c63014eeff0ead443
bernadette-eu <bernadette.aueb@gmail.com>
2021-06-22 17:55:49 +0300

    Added Stan functionality

---
+++ R/Bernadette-package.R
@@ -0,0 +1,19 @@
+#' Bayesian inference and model selection for stochastic epidemics
+#'
+#' \code{Bernadette} provides Bayesian analysis for stochastic extensions of non-linear dynamic systems using advanced computational algorithms.
+#'
+#' @docType package
+#' @name Bernadette-package
+#' @aliases Bernadette
+#' @useDynLib bernadette, .registration = TRUE
+#' @import bayesplot
+#' @import ggplot2
+#' @import lubridate
+#' @import methods
+#' @import Rcpp
+#' @import RcppParallel
+#' @import readr
+#' @import rstan
+#' @import scoringRules
+#' @import vroom
+NULL
