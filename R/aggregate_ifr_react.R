0e2272e08e60e0cdf12c66fcf0b6e0e0a5b36458
bernadette-eu <bernadette.aueb@gmail.com>
2022-03-18 17:49:10 +0200

    Fixed calculation of weighted average

--- R/aggregate_ifr_react.R
+++ R/aggregate_ifr_react.R
@@ -30,54 +30,55 @@
 #'}
 #'
 #' @export
-aggregate_ifr_react <- function(x, 
+aggregate_ifr_react <- function(x,
                                 user_AgeGrp){
-  
+
   options(dplyr.summarise.inform = FALSE)
-  
+
   react_AgeGrp <- c("15-44", "45-64", "65-74", "75+")
-  
+
   if( length(user_AgeGrp$REACT) == length(react_AgeGrp) &
       isTRUE(all.equal(user_AgeGrp$REACT, react_AgeGrp)) == FALSE ) stop("Provide mapping for the REACT study age groups [15-44, 45-64, 65-74, 75+].")
-  
+
   temp_x <- x
   temp_x$AgeGrp    <- gsub("\\+", "-100", temp_x$AgeGrp)
   temp_x$AgeGrpEnd <- sapply(1:nrow(temp_x), function(x){max(as.numeric(strsplit(temp_x$AgeGrp, "-")[[x]]))})
-  
+
   ifr_react <- data.frame(AgeGrp = c("15-44", "45-64", "65-74", "75-100"),
                           IFR    = c(0.03, 0.52, 3.13, 11.64)/100)
   ifr_react$AgeGrpStart <- sapply(1:nrow(ifr_react), function(x){min(as.numeric(strsplit(ifr_react$AgeGrp, "-")[[x]]))})
   ifr_react$AgeGrpEnd   <- sapply(1:nrow(ifr_react), function(x){max(as.numeric(strsplit(ifr_react$AgeGrp, "-")[[x]]))})
 
-  ifr_react$PopPerc <- 0
-  
+  #ifr_react$PopPerc <- 0
+
   for (i in 1:nrow(ifr_react)){
-    
+
     if (i == 1) select_indx <- temp_x$AgeGrpEnd <= ifr_react$AgeGrpEnd[i]
-    else select_indx <- (temp_x$AgeGrpStart >= ifr_react$AgeGrpStart[i]) & 
+    else select_indx <- (temp_x$AgeGrpStart >= ifr_react$AgeGrpStart[i]) &
                         (temp_x$AgeGrpEnd   <= ifr_react$AgeGrpEnd[i])
-    
-    ifr_react$PopPerc[i] <- sum(temp_x$PopTotal[select_indx])/sum(temp_x$PopTotal)
+
+    ifr_react$PopN[i] <- sum(temp_x$PopTotal[select_indx])#/sum(temp_x$PopTotal)
 
   }# End for
-  
+
   ifr_react$AgeGrp <- gsub("\\-100", "+", ifr_react$AgeGrp)
-  
+
   output <- list()
-  
+
   output[[1]] <- ifr_react
-  
-  output[[2]] <- ifr_react %>% as.data.frame() %>% 
-                 dplyr::left_join(user_AgeGrp, 
-                                  by = c("AgeGrp" = "REACT")) %>% 
-                 dplyr::group_by(User) %>% 
-                 dplyr::mutate(AgrIFR = sum(IFR*PopPerc)) %>% 
+
+  output[[2]] <- ifr_react %>% as.data.frame() %>%
+                 dplyr::left_join(user_AgeGrp,
+                                  by = c("AgeGrp" = "REACT")) %>%  # merge(x = ifr_react, y = user_AgeGrp, by = c("AgeGrp" = "REACT"), all.x = TRUE)
+                 dplyr::group_by(User) %>%
+                 dplyr::mutate(PopPerc = prop.table(PopN),
+                               AgrIFR = sum(IFR*PopPerc)) %>%
                  dplyr::rename(REACT_AgeGrp = AgeGrp,
-                               User_AgeGrp  = User) %>% 
+                               User_AgeGrp  = User) %>%
                  dplyr::select(dplyr::one_of(c("User_AgeGrp", "AgrIFR")) ) %>%
-                 dplyr:: group_by(User_AgeGrp, AgrIFR) %>%
+                 dplyr::group_by(User_AgeGrp, AgrIFR) %>%
                  dplyr::slice(1) %>% as.data.frame()
-  
+
   return(output)
 }
 
